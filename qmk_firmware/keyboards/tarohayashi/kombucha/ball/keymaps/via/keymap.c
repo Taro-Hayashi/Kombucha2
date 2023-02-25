ed20303dab9dcf11a091e6d5f6541f0597fadce8
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2023-02-25 21:12:09 +0900

    Update keymap.c

--- qmk_firmware/keyboards/tarohayashi/kombucha/ball/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/tarohayashi/kombucha/ball/keymaps/via/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, MO(UPPER), XXXXXXX
     ),
   [LIGHT] = LAYOUT(
-        XXXXXXX, XXXXXXX,
+        RGB_TOG, XXXXXXX,
         RGB_SAI, RGB_VAI, RGB_SPI,
         RGB_SAD, RGB_VAD, RGB_SPD,
         RGB_MOD, RGB_RMOD, LT(LIGHT, KC_K)
