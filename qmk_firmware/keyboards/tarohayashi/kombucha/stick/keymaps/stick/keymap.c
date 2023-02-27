72da45530a2a7b51276523118786e25e72ba3cc5
Taro Hayashi <87086895+Taro-Hayashi@users.noreply.github.com>
2023-02-27 17:58:50 +0900

    Update keymap.c

--- qmk_firmware/keyboards/tarohayashi/kombucha/stick/keymaps/stick/keymap.c
+++ qmk_firmware/keyboards/tarohayashi/kombucha/stick/keymaps/stick/keymap.c
@@ -65,71 +65,63 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 };
 
 
-keyevent_t x_hi = {
-    .key = (keypos_t){.row = 5, .col = 1},
-    .pressed = false
-};
+keypos_t key_up;
+keypos_t key_left;
+keypos_t key_right;
+keypos_t key_down;
+bool pressed_up;
+bool pressed_down;
+bool pressed_left;
+bool pressed_right;
 
-keyevent_t x_low = {
-    .key = (keypos_t){.row = 5, .col = 2},
-    .pressed = false
-};
-
-keyevent_t y_hi = {
-    .key = (keypos_t){.row = 5, .col = 0},
-    .pressed = false
-};
-
-keyevent_t y_low = {
-    .key = (keypos_t){.row = 5, .col = 3},
-    .pressed = false
-};
+void matrix_init_user(void) {
+    key_up.row = 5;
+    key_up.col = 0;
+    key_left.row = 5;
+    key_left.col = 1;
+    key_right.row = 5;
+    key_right.col = 2;
+    key_down.row = 5;
+    key_down.col = 3;
+}
 
 void matrix_scan_user(void) {
-
-    if (IS_PRESSED(x_hi)) {
-        x_hi.pressed = false;
-        x_hi.time = (timer_read() | 1);
-        action_exec(x_hi);
-    }
-    if (IS_PRESSED(x_low)) {
-        x_low.pressed = false;
-        x_low.time = (timer_read() | 1);
-        action_exec(x_low);
-    }
-    if (IS_PRESSED(y_hi)) {
-        y_hi.pressed = false;
-       y_hi.time = (timer_read() | 1);
-        action_exec(y_hi);
-    }
-    if (IS_PRESSED(y_low)) {
-        y_low.pressed = false;
-        y_low.time = (timer_read() | 1);
-        action_exec(y_low);
-    }
-
     int16_t x = analogReadPin(GP27);
     int16_t y = analogReadPin(GP28);
-    if(x > 920){
-        x_hi.pressed = true;
-        x_hi.time = (timer_read() | 1);
-        action_exec(x_hi);
-        wait_ms(5);
-    }else if(x< 550){
-        x_low.pressed = true;
-        x_low.time = (timer_read() | 1);
-        action_exec(x_low);
-        wait_ms(5);
+    int8_t layer = layer_switch_get_layer(key_up);
+    int16_t keycode_up = keymap_key_to_keycode(layer, key_up);
+    int16_t keycode_left = keymap_key_to_keycode(layer, key_left);
+    int16_t keycode_right = keymap_key_to_keycode(layer, key_right);
+    int16_t keycode_down = keymap_key_to_keycode(layer, key_down);
+
+    if(pressed_left == false && x > 920){
+        register_code(keycode_left);
+        pressed_left = true;
+    }else if(pressed_left == true && x <920){
+        unregister_code(keycode_left);
+        pressed_left = false;
     }
-    if(y > 920){
-        y_hi.pressed = true;
-        y_hi.time = (timer_read() | 1);
-        action_exec(y_hi);
-        wait_ms(5);
-    }else if(y < 550){
-        y_low.pressed = true;
-        y_low.time = (timer_read() | 1);
-        action_exec(y_low);
-        wait_ms(5);
-     }
+    if(pressed_right == false && x < 550){
+        register_code(keycode_right);
+        pressed_right = true;
+    }else if (pressed_right == true && x >550){
+        unregister_code(keycode_right);
+        pressed_right = false;
+    }
+
+    if(pressed_up == false && y > 920){
+        register_code(keycode_up);
+        pressed_up = true;
+    }else if(pressed_up == true && y < 920){
+        unregister_code(keycode_up);
+        pressed_up = false;
+    }
+
+    if(pressed_down == false && y < 550){
+        register_code(keycode_down);
+        pressed_down = true;
+    }else if(pressed_down == true && y > 550){
+        unregister_code(keycode_down);
+        pressed_down = false;
+    }
 }
